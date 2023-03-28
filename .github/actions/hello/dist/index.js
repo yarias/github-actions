7482e0930c6002a9cde4b72f32a2d53f9f9757d9
Yermi Arias <yermi.arias.alfaro@kyndryl.com>
2023-03-28 16:44:44 -0600

    feat: extra use of actions/core

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1225,13 +1225,27 @@ const github = __webpack_require__(469)
 
 try {
     // throw( new Error("some error message"))
+
+    core.debug('Debug message')
+    core.warning('warning message')
+    core.error('error message')
+
     const name = core.getInput('who-tp-greet')
+    core.setSecret(name) // create a github secret
     console.log(`Hello ${name}`)
+
     
     const time = new Date()
     core.setOutput('time', time.toTimeString())
     
+    // group logs 
+    core.startGroup('Loggin github object')
     console.log(JSON.stringify(github, null, '\t'))
+    core.endGroup()
+
+    // create environments
+    core.exportVariable('HELLO', "hello")
+    
 } catch(error)
 {
     core.setFailed(error.message)
